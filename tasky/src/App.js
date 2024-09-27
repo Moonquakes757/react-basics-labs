922e8db4cef0a40dbbdf0d27019c4a990846cc85
Moonquakes <Zhuanz@MacBook-Air.local>
2024-09-27 13:53:26 +0100

    Completed Tasky Lab 1

---
+++ tasky/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Task from './components/Task';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Tasky</h1>
+      <Task title="Dishes" deadline="Today" description="Empty dishwasher"></Task>
+      <Task title="Laundry" deadline="Tomorrow" description="Fold laundry and put away"></Task>
+      <Task title="Tidy" deadline="Today"/>
+    </div>
+  );
+}
+
+export default App;
